69e7f16ed4bf7c812d6ab0797f93256224c20cad
itsme-swe <me.harshmehra07@gmail.com>
2025-04-07 17:10:19 +0530

    update

--- Season1_Topics_Apr25/02_Modules/3_nestedModules/app.js
+++ Season1_Topics_Apr25/02_Modules/3_nestedModules/app.js
@@ -1,5 +1,7 @@
 const { calSum, calMultiplication } = require("./calculate");
 
+const data = require("./data.json"); // importing json file
+
 let a = 10;
 
 let b = 20;
@@ -7,3 +9,5 @@ let b = 20;
 calMultiplication(a, b); //◽ 200
 
 calSum(a, b); //◽ 30
+
+console.log(JSON.stringify(data)); //◽ {"name":"Harsh","city":"jaipur","country":"India"}
