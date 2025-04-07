bf1057d24abd3a8b40153a312a65b8724f279648
itsme-swe <me.harshmehra07@gmail.com>
2025-04-07 05:47:12 +0530

    update

--- Season1_Topics_Apr25/02_Modules/app.js
+++ Season1_Topics_Apr25/02_Modules/app.js
@@ -2,6 +2,6 @@
 💥 Generally "app.js" use to be the entry point in node application.
 */
 
-const math = require("./math"); // ◽ require() is an function used to import one module into another
+const { add } = require("./math"); // ◽ require() is an function used to import one module into another
 
-console.log(math.add(20, 20));
+console.log(add(20, 20)); // 40
