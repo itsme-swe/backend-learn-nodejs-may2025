8db892e1c77a4b4579db40a2b1a36a2e1f5faf60
itsme-swe <me.harshmehra07@gmail.com>
2025-04-07 16:47:01 +0530

    update

--- Season1_Topics_Apr25/02_Modules/1_commonJS_cjsPattern/math.js
+++ Season1_Topics_Apr25/02_Modules/1_commonJS_cjsPattern/math.js
@@ -4,6 +4,10 @@ function add(a, b) {
 
 const msg = "Hello World";
 
+console.log(module.exports);  //◽ {}
+
 module.exports = { msg, add };
+
+console.log(module.exports);  //◽ { msg: 'Hello World', add: [Function: add] }
 
 //◽ Exporting "add()" function and "msg" variable by using commonJs feature "module.exports"
