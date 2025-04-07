76992b21bb6643479ccbcb0e60e537448b063cf2
itsme-swe <me.harshmehra07@gmail.com>
2025-04-07 16:26:40 +0530

    update

--- Season1_Topics_Apr25/02_Modules/2_ESModules_mjs/app.js
+++ Season1_Topics_Apr25/02_Modules/2_ESModules_mjs/app.js
@@ -1,3 +1,10 @@
 import { add } from "./math.js";
 
 console.log(add(20, 30));
+
+/*
+💥 To enable ES Module we need to make some changes inside our 'package.json' file. In node by default 'cjs' is enabled to switch to 'mjs' we need to change:
+    {
+      "type" : "module"
+    }
+*/
