412a3d1c340fd5030c450a6bfb5a4db3174b924f
itsme-swe <me.harshmehra07@gmail.com>
2025-04-09 17:05:42 +0530

    update

---
+++ Season1_Topics_Apr25/03_TheWayCodeRuns/1_inSynchronousMode/sync.js
@@ -0,0 +1,22 @@
+console.log("Node Starting");
+
+let a = 19081994;
+let b = 5061992;
+
+function multiplyFn(x, y) {
+  const val = a * b;
+  console.log(val);
+  return val;
+}
+
+let c = multiplyFn(a, b);
+
+console.log("The value of c is: ", c);
+
+/*
+◽ output ⇨
+Node Starting
+96592900972048
+The value of c is:  96592900972048
+
+*/
