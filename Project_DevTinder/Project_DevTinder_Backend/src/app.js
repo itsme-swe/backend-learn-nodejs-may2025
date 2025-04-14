ff60f49b961bf82f62149256539ad0b3f448ce43
itsme-swe <me.harshmehra07@gmail.com>
2025-04-14 22:35:13 +0530

    update

---
+++ Project_DevTinder/Project_DevTinder_Backend/src/app.js
@@ -0,0 +1,21 @@
+/*
+💥 Now we are creating server using express.js 
+ */
+
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my world");
+});
+
+app.get("/dev", (req, res) => {
+  res.send("I am SDE Harsh.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}.`);
+});
