dfc8f1e0c91a477ab38692aa4c61fd0f57ad6ed3
itsme-swe <me.harshmehra07@gmail.com>
2025-04-18 15:32:44 +0530

    update

--- Project_DevTinder/Project_DevTinder_Backend/src/app.js
+++ Project_DevTinder/Project_DevTinder_Backend/src/app.js
@@ -6,11 +6,14 @@ const express = require("express");
 
 const { adminAuth } = require("./middlewares/auth");
 
-const { connectDB } = require("./config/database");
+const connectDB = require("./config/database");
+
+const User = require("./models/user");
 
 const app = express();
 const PORT = 3000;
 
+//◽ Connecting our app to DB
 connectDB()
   .then(() => {
     console.log("Database connection established...");
@@ -23,57 +26,71 @@ connectDB()
   });
 
 //◽ Accessing query parametrs
-app.get("/dev", (req, res) => {
-  const query = req.query.username;
-  const pass = req.query.pass;
+// app.get("/dev", (req, res) => {
+//   const query = req.query.username;
+//   const pass = req.query.pass;
 
-  res.send(`Searching for: ${query} and ${pass}`);
-});
+//   res.send(`Searching for: ${query} and ${pass}`);
+// });
 
 //◽ Route parameters
-app.get("/user/:userId/:password", (req, res) => {
-  const userId = req.params.userId;
-  const pass = req.params.password;
+// app.get("/user/:userId/:password", (req, res) => {
+//   const userId = req.params.userId;
+//   const pass = req.params.password;
 
-  console.log(userId);
-  console.log(pass);
+//   console.log(userId);
+//   console.log(pass);
 
-  res.send(`UserId is ${userId} and password is ${pass}`);
-});
+//   res.send(`UserId is ${userId} and password is ${pass}`);
+// });
 
-app.post("/user", (req, res) => {
-  res.send("Data Successfully saved to DB.");
-});
+// app.post("/user", (req, res) => {
+//   res.send("Data Successfully saved to DB.");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("User deleted successfully");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("User deleted successfully");
+// });
 
 //◽ Using regex
-app.get(/\/ab?cd/, (req, res) => {
-  res.send("ab?cd");
-});
+// app.get(/\/ab?cd/, (req, res) => {
+//   res.send("ab?cd");
+// });
 
 //◽ Multiple route handler
-app.get(
-  "/car",
-  (req, res, next) => {
-    console.log("Handling the route car_1.");
-    next();
-  },
-  (req, res) => {
-    console.log("Handling the car_2");
-    res.send("2nd Handler responding");
-  }
-);
+// app.get(
+//   "/car",
+//   (req, res, next) => {
+//     console.log("Handling the route car_1.");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("Handling the car_2");
+//     res.send("2nd Handler responding");
+//   }
+// );
 
 //💥 Writing Middleware for all GET, POST, DELETE,.... requests
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data sent");
-});
+// app.get("/admin/getAllData", (req, res) => {
+//   res.send("All Data sent");
+// });
 
-app.delete("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
+// app.delete("/admin/deleteUser", (req, res) => {
+//   res.send("Deleted a user");
+// });
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Juhu",
+    lastName: "Chellani",
+    emailId: "juhi123@xyz",
+    password: "abc123",
+    age: 30,
+    gender: "Female",
+  });
+
+  await user.save();
+  res.send("User added successfully");
 });
--- Project_DevTinder/Project_DevTinder_Backend/src/config/database.js
+++ Project_DevTinder/Project_DevTinder_Backend/src/config/database.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 //◽ Always use async await bcoz it return us promise
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://meharsh05:dollarkamao@harsh.rxfdaal.mongodb.net/?retryWrites=true&w=majority&appName=Harsh/DevTinder_DevBuild"
+    "mongodb+srv://meharsh05:dollarkamao@harsh.rxfdaal.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Harsh"
   );
 };
 
-module.exports = { connectDB };
+module.exports = connectDB;
