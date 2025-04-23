54147353ad5dd18067b419668859109b149f8d01
itsme-swe <me.harshmehra07@gmail.com>
2025-04-23 18:51:43 +0530

    update

--- Project_DevTinder/Project_DevTinder_Backend/src/models/user.js
+++ Project_DevTinder/Project_DevTinder_Backend/src/models/user.js
@@ -3,21 +3,36 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
   },
   gender: {
     type: String,
+  },
+  photoUrl: {
+    type: String,
+  },
+  bio: {
+    type: String,
+    default: "Please add short bio about yourself!",
+  },
+  skills: {
+    type: [String],
   },
 });
 
