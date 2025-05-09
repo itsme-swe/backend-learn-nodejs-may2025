b5d7eec1bb31419704621c56a84d8c7cb59dfc1e
harsh_sde <harsh_s.dev@outlook.com>
2025-05-09 13:13:47 +0530

    connectionReq schema updated

--- Project_DevTinder/Project_DevTinder_Backend/src/models/connectionReq.js
+++ Project_DevTinder/Project_DevTinder_Backend/src/models/connectionReq.js
@@ -6,12 +6,15 @@ const connectionReqSchema = new mongoose.Schema(
   {
     fromUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      require: true,
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
+      require: true,
     },
     status: {
       type: String,
+      require: true,
       enum: {
         values: ["ignore", "intrested", "accepted", "rejected"],
         message: `${VALUE} is incorrect status type`,
