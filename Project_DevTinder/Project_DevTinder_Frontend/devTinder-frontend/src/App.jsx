87430480974480f00614293125885524254091a8
harsh_sde <harsh_s.dev@outlook.com>
2025-05-30 06:02:26 +0530

    update

--- Project_DevTinder/Project_DevTinder_Frontend/devTinder-frontend/src/App.jsx
+++ Project_DevTinder/Project_DevTinder_Frontend/devTinder-frontend/src/App.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
-import Body from "./Body";
-import Login from "./Login";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Feed from "./components/Feed";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Profile from "./Profile";
+import Profile from "./components/Profile";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 
@@ -13,6 +14,7 @@ function App() {
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
             </Route>
--- Project_DevTinder/Project_DevTinder_Frontend/devTinder-frontend/src/components/Login.jsx
+++ Project_DevTinder/Project_DevTinder_Frontend/devTinder-frontend/src/components/Login.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { addUser } from "./utils/userSlice";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 function Login() {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   //💥 Function will handle login network call
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:3000/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
-      dispatch(addUser(res.data));  // adding user to redux store inside 
+      dispatch(addUser(res.data)); // adding user to redux store inside
+      return navigate("/");
     } catch (err) {
       console.error(err);
     }
